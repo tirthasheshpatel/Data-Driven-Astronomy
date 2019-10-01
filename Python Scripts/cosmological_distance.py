c6d76bc7335509435b5fffdf313a7affc90feea1
tirthasheshpatel <tirthasheshpatel@gmail.com>
2019-10-01 18:19:22 +0530

    Add data to data files and update scripts

--- Python Scripts/cosmological_distance.py
+++ Python Scripts/cosmological_distance.py
@@ -13,6 +13,24 @@ def angular_dist(r1, d1, r2, d2):
     b = np.cos(np.radians(d1))*np.cos(np.radians(d2))*np.sin(np.radians(abs(r1-r2)/2.))**2
     return 2.*np.degrees(np.arcsin(np.sqrt(a + b)))
 
+def import_bss():
+    cat = np.loadtxt('cross-matching/bss.dat', usecols=range(1,7))
+    return cat
+
+def import_super():
+    cat = np.loadtxt('cross-matching/super.csv', delimiter=',', skiprows=1, usecols=[0, 1])
+    return cat
+
+def find_closest(cat, targetRA, targetD):
+    closestID, closest = ( 0, angular_dist(hms2dec(*cat[0][0:3]), dms2dec(*cat[0][3:6]), targetRA, targetD) )
+    for i in range(cat.shape[0]):
+        current = angular_dist(hms2dec(*cat[i][0:3]), dms2dec(*cat[i][3:6]), targetRA, targetD)
+        if current<closest:
+            closestID = i
+            closest = current
+    return closestID+1, closest
+
+
 if __name__ == '__main__':
     print(hms2dec(23, 12, 6))
 
@@ -23,3 +41,9 @@ if __name__ == '__main__':
     print(angular_dist(21.07, 0.1, 21.15, 8.2))
 
     print(angular_dist(10.3, -3, 24.3, -29))
+
+    cat = import_bss()
+
+    print(find_closest(cat, 175.3, -32.5))
+
+    print(find_closest(cat, 32.2, 40.7))
