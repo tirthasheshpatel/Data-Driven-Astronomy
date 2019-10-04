d1783d1dd1fefa56399c0a8effa491d08f8704fc
tirthasheshpatel <tirthasheshpatel@gmail.com>
2019-10-04 14:23:13 +0530

    Add vectorized approach to find closest

--- Python Scripts/cosmological_distance.py
+++ Python Scripts/cosmological_distance.py
@@ -10,36 +10,91 @@ Our bss.dat file contains 320 data-points in the form: rah, ram, ras, dd, dm, ds
 where rah = right ascension hour, ram = right ascension minute, etc
 Similarly, super.csv contains 500 data-points of same form but from a different catalogue!
 """
+
+# importing numpy for mathematical calculations
 import numpy as np
 
+# We need to convert the HMS form to floating points so
+# that we can calulate the algular distance between
+# two objects.
 def hms2dec(h,m,s):
+    """
+    Converts HMS (hour minute second) to floating point.
+    >>> h,m,s = 4, 6.6, 34
+    >>> hms2dec(h,m,s)
+    61.791666666666664
+    """
     return 15.*(h + m/60. + s/3600.)
 
+# We need to convert the DMS form to floating points so
+# that we can calulate the algular distance between
+# two objects.
 def dms2dec(d,m,s):
+    """
+    Converts DMS (degree minute second) to floating points.
+    >>> d,m,s = -16, 36.5, 4.4
+    >>> dms2dec(d,m,s)
+    -16.609555555555556
+    """
     if d < 0:
         return -1*(abs(d) + m/60. + s/3600.)
     return d + m/60. + s/3600.
 
+
+# We need angular distance to cross-match two catalouges.
 def angular_dist(r1, d1, r2, d2):
+    """
+    Calculates the projected angular distance between two
+    astronomical objects on the cosmological sphere using
+    haversine formula.
+    Accepts as arguments the right ascention and declination
+    of two objects between which distance is to be evaluated.
+    >>> r1, d1, r2, d2 = 10.2, 12.3, 6.4, -8.8
+    >>> angular_dist(r1, d1, r2, d2)
+    21.435312977951494
+    """
     a = np.sin(np.radians(abs(d1-d2)/2.))**2
     b = np.cos(np.radians(d1))*np.cos(np.radians(d2))*np.sin(np.radians(abs(r1-r2)/2.))**2
     return 2.*np.degrees(np.arcsin(np.sqrt(a + b)))
 
 def import_bss():
+    """
+    Importing the BSS dataset.
+    >>> dataset = import_bss()
+    """
     cat = np.loadtxt('cross-matching/bss.dat', usecols=range(1,7))
     return cat
 
 def import_super():
+    """
+    Imorting the dataset.
+    >>> dataset = import_super()
+    """
     cat = np.loadtxt('cross-matching/super.csv', delimiter=',', skiprows=1, usecols=[0, 1])
     return cat
 
 def find_closest(cat, targetRA, targetD):
+    """
+    Finds the distace between objects to be detected and to be
+    validated from the catalogue.
+    >>> find_closest(cat, 10.0, 20.0)
+    """
+
+    # Initialize the ID and distance using first object in the catalogue
     closestID, closest = ( 0, angular_dist(hms2dec(*cat[0][0:3]), dms2dec(*cat[0][3:6]), targetRA, targetD) )
+
+    # Iterate through the entire catalogue and
+    # find the distance between all the objects
+    # choosing the least found distance.
+    # Note: Can be vectorized!
     for i in range(cat.shape[0]):
         current = angular_dist(hms2dec(*cat[i][0:3]), dms2dec(*cat[i][3:6]), targetRA, targetD)
         if current<closest:
             closestID = i
             closest = current
+    # Vectorized approach
+    # dist_array = angular_dist(hms2dec(cat[:][0], cat[:][1], cat[:][2]), dms2dec(cat[:][3], cat[:][4], cat[:][5]), targetRA, targetD)
+    # (closestID, closest) = (np.argmin(dist_array)+1, np.min(dist_array))
     return closestID+1, closest
 
 def crossmatch(cat1, cat2, radius):
@@ -91,3 +146,4 @@ if __name__ == '__main__':
     print(matches[:3])
     print(no_matches[:3])
     print(len(no_matches))
+
